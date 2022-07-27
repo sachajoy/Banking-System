28f478853471e4b2ce0ab77c93152761961a940e
Arihant Banthia (195259) <arihanba@amdocs.com>
2022-07-27 17:15:05 +0530

    Created the controller for the application

---
+++ Banking-System/src/main/java/com/banking/bankingsystem/controller/DepositMoneyController.java
@@ -0,0 +1,63 @@
+package com.banking.bankingsystem.controller;
+
+import com.banking.bankingsystem.model.User;
+import com.banking.bankingsystem.pojo.DepositAmount;
+import com.banking.bankingsystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DepositMoneyController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(value = "/deposit")
+    public ModelAndView deposit(DepositAmount depositAmount) {
+      ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByUsername(auth.getName());
+        int balance = user.getBalance();
+        if ( depositAmount.getDepositAmount() < 1) {
+            modelAndView.addObject("errorMessage", "Amount Cannot to less than one");
+            modelAndView.addObject("userDetails", user);
+            modelAndView.setViewName("home");
+            return modelAndView;
+        }
+        balance -= depositAmount.getDepositAmount();
+        user.setBalance(balance);
+        userService.saveUser(user);
+        modelAndView.addObject("successMessage", "Amount Deposited");
+        modelAndView.addObject("userDetails", user);
+        modelAndView.setViewName("home");
+        return modelAndView;
+    }
+
+    @PostMapping(value = "/withdraw")
+    public ModelAndView withdraw(DepositAmount depositAmount) {
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByUsername(auth.getName());
+        int balance = user.getBalance();
+        if (balance < depositAmount.getDepositAmount()) {
+            modelAndView.addObject("errorMessage", "Cannot withdraw amount more than your balance");
+            modelAndView.addObject("userDetails", user);
+            modelAndView.setViewName("home");
+            return modelAndView;
+        }
+        balance += depositAmount.getDepositAmount();
+        user.setBalance(balance);
+        userService.saveUser(user);
+        modelAndView.addObject("successMessage", "Amount Deposited");
+        modelAndView.addObject("userDetails", user);
+        modelAndView.setViewName("home");
+        return modelAndView;
+    }
+}
---
+++ Banking-System/src/main/java/com/banking/bankingsystem/controller/LoginAndRegistrationController.java
@@ -0,0 +1,69 @@
+package com.banking.bankingsystem.controller;
+
+import com.banking.bankingsystem.model.User;
+import com.banking.bankingsystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class LoginAndRegistrationController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = {"/", "/login"})
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+    @GetMapping(value="/registration")
+    public ModelAndView registration(){
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("registration");
+        return modelAndView;
+    }
+
+    @PostMapping(value = "/registration")
+    public ModelAndView createNewUser(User user, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        User userExists = userService.findUserByUsername(user.getUsername());
+        if (userExists != null) {
+            bindingResult
+                    .rejectValue("username", "error.user",
+                            "There is already a user registered with the user name provided");
+        }
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("registration");
+        } else {
+            user.setAccountNumber(String.valueOf(Math.random() * ((10000000 - 9999999)+ 1) + 9999999));
+            user.setBalance(0);
+            userService.saveUser(user);
+            modelAndView.addObject("successMessage", "User has been registered successfully");
+            modelAndView.addObject("user", new User());
+            modelAndView.setViewName("registration");
+
+        }
+        return modelAndView;
+    }
+    @GetMapping(value="/home")
+    public ModelAndView home(){
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByUsername(auth.getName());
+        modelAndView.addObject("userDetails", user);
+        modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
+        modelAndView.setViewName("home");
+        return modelAndView;
+    }
+
+}
